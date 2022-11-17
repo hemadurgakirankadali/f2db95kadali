dbfc7ad70496f1b1183e563ce8a38bd25af16df5
hemadurgakirankadali <112112171+hemadurgakirankadali@users.noreply.github.com>
2022-11-16 23:12:26 -0600

    bouns

--- controllers/earbuds.js
+++ controllers/earbuds.js
@@ -33,12 +33,20 @@ exports.earbuds_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 };  
-// Handle Costume delete form on DELETE. 
-exports.earbuds_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: earbuds delete DELETE ' + req.params.id); 
-}; 
+// Handle earbuds delete form on DELETE. 
+exports.earbuds_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await earbuds.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+    };
  
-// Handle Costume update form on PUT. 
+// Handle earbuds update form on PUT. 
 exports.earbuds_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: earbuds update PUT' + req.params.id); 
 };
@@ -76,3 +84,91 @@ exports.earbuds_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 };
+
+// for a specific Costume.
+exports.earbuds_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await earbuds.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+
+    // Handle Costume update form on PUT.
+exports.earbuds_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await earbuds.findById( req.params.id)
+// Do updates of properties
+if(req.body.earbuds_company)
+toUpdate.earbuds_company = req.body.earbuds_company;
+if(req.body.earbuds_cost) toUpdate.earbuds_cost = req.body.earbuds_cost;
+if(req.body.earbuds_Colour) toUpdate.earbuds_Colour = req.body.earbuds_Colour;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
+
+// Handle a show one view with id specified by query
+exports.earbuds_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await earbuds.findById( req.query.id)
+    res.render('earbudsdetail',
+    { title: 'earbuds Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+ // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.earbuds_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('earbudscreate', { title: 'Earbuds Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.earbuds_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await earbuds.findById(req.query.id)
+    res.render('earbudsupdate', { title: 'Earbuds Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle a delete one view with id from query
+exports.earbuds_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await earbuds.findById(req.query.id)
+    res.render('earbudsdelete', { title: 'Earbuds Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
--- routes/earbuds.js
+++ routes/earbuds.js
@@ -1,10 +1,22 @@
 var express = require('express');
 const earbuds_controlers= require('../controllers/earbuds'); 
 var router = express.Router();
+
+// GET request for one costume.
+router.get('/earbuds/:id', earbuds_controlers.earbuds_detail);
  
 
 /* GET home page. */
 
 router.get('/', earbuds_controlers.earbuds_view_all_Page ); 
+
+/* GET detail earbuds page */
+router.get('/detail', earbuds_controlers.earbuds_view_one_Page);
+/* GET create costume page */
+router.get('/create', earbuds_controlers.earbuds_create_Page);
+/* GET create update page */
+router.get('/update', earbuds_controlers.earbuds_update_Page);
+/* GET delete costume page */
+router.get('/delete', earbuds_controlers.earbuds_delete_Page);
 
 module.exports = router;
